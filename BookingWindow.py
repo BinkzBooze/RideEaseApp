2135f4b77933b427026548183a4f6635afe9c5f1
BinkzBooze <164988611+BinkzBooze@users.noreply.github.com>
2024-07-02 11:47:47 +0800

    Update BookingWindow.py

--- BookingWindow.py
+++ BookingWindow.py
@@ -6,13 +6,12 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import webbrowser
 import json
-import re
 import requests
 from Vehicle_Classes import Vehicle, Motorcycle, Car, Van
 from BookingsClass import Booking
 from datetime import datetime
 import requests
-
+import glob
 
 def booking_window_open():
 
@@ -23,6 +22,7 @@ def booking_window_open():
     api_key = "AIzaSyDVMXyo9XYUdOBjr9Kv0TLbpQiDVv2lds0" 
 
     def get_distance(pickup_location, dropoff_location, api_key):
+        #Get the estimated distance travelled based on common routes.
         url = "https://maps.googleapis.com/maps/api/distancematrix/json"
         params = {
             "origins": pickup_location,
@@ -42,37 +42,45 @@ def booking_window_open():
             response.raise_for_status()
 
     def load_current_user():
+        #Identifies which account is logged in.
         with open("Accounts.json", "r") as file:
             data = json.load(file)
         return data["current_user"]
 
-    def switch_to_booking_tab():
-        notebook.select(booking_tab)
+# ---------------------------------------------------------------------------- #
+#                         FUNCTIONS FOR SWITCHING TABS                         #
+# ---------------------------------------------------------------------------- #
 
     def switch_to_home_tab():
+        #Switches to home page
         notebook.select(home_tab)
 
+    def switch_to_booking_tab():
+        #Switches to booking tab
+        notebook.select(booking_tab)
+
+    def switch_to_booking_successful_tab():
+
     def switch_to_bookings_list_tab():
+        #Switches to booking lists tab.
         notebook.select(bookings_list_tab)
 
+# ---------------------------------------------------------------------------- #
+#                             FUNCTIONS FOR BUTTONS                            #
+# ---------------------------------------------------------------------------- #
+
     def motorcycle_button_clicked(event):
-        global passenger_limit
         global vehicle
-        passenger_limit = 1
         vehicle = Motorcycle()
         switch_to_booking_tab()
 
     def car_button_clicked(event):
-        global passenger_limit
         global vehicle
-        passenger_limit = 6
         vehicle = Car()
         switch_to_booking_tab()
 
     def van_button_clicked(event):
-        global passenger_limit
         global vehicle
-        passenger_limit = 12
         vehicle = Van()
         switch_to_booking_tab()
     
@@ -93,10 +101,7 @@ def booking_window_open():
         except ValueError:
             messagebox.showerror("Error", "Pax must be a valid integer.")
             return
-
-        # Handle successful booking
-    def switch_to_booking_successful_tab():
-
+        
     def go_back_to_home_page_button_clicked(event):
         switch_to_home_tab()
         #####################gamitin to para sa button ng 3rd tab pati na rin dun sa gilid na icon############
@@ -109,23 +114,56 @@ def booking_window_open():
         selected_item = bookings_treeview.focus()
         if selected_item:
             bookings_treeview.item(selected_item, values=("Cancelled",))
-        
+            cancel_booking()
+
+# ---------------------------------------------------------------------------- #
+#                       FUNCTIONS FOR VALIDATION OF INPUS                      #
+# ---------------------------------------------------------------------------- #
+
     def validate_pickup_location():
         #Validates the pick-up address if it exists.
-
 
     def validate_dropoff_location():
         #Validates the drop off address if it exists.
 
-    def validate_passenger_amount():
-        #Validates if pax entered is less than or equal the capacity of selected vehicle.
+    def validate_passenger_amount(passengers):
+        # Validates if pax entered is less than or equal the capacity of selected vehicle.
+        try:
+            passenger_count = int(passengers)
+        except ValueError:
+            messagebox.showerror("Error", "Passenger count should be a numeric value.", parent=booking_window)
+            return
+
+        if passenger_count <= 0:
+            messagebox.showerror("Error", "Passenger count should be a positive integer.", parent=booking_window)
+            return
+
+        #Car's recommended capacity is 4, allow up to 6 passengers only.
+        if isinstance(vehicle, Car) and 4 < passenger_count <= 6:
+            if not messagebox.askyesno("Confirmation", "The number of passengers exceeds the car's capacity. Are you sure you want to continue?", parent=booking_window):
+                return
+
+        #Van's recommended capacity is 10, allow up to 12 passengers only.
+        elif isinstance(vehicle, Van) and 10 < passenger_count <= 12:
+            if not messagebox.askyesno("Confirmation", "The number of passengers exceeds the van's capacity. Are you sure you want to continue?", parent=booking_window):
+                return
+
+        #Error if passenger amount is greater than vehicle capacity.
+        elif passenger_count > vehicle.capacity:
+            messagebox.showerror("Error", "The selected vehicle does not have enough capacity for the number of passengers.", parent=booking_window)
+            return
 
 
-    def book_now(pickup_location, dropoff_location, passenger_amount)
+# ---------------------------------------------------------------------------- #
+#                             FUNCTIONS FOR BOOKING                            #
+# ---------------------------------------------------------------------------- #
+
+
+    def book_now(username, pickup, dropoff, date_and_time, pax, vehicle_type, total_distance, total_cost, status):
         #validates booking information and creates a new booking if valid.
-        valid_pickup_location, pickup_msg = validate_pickup_location(pickup_location)
-        valid_dropoff_location, dropoff_msg = validate_dropoff_location(dropoff_location)
-        valid_passenger_amount, pax_msg = validate_passenger_amount(passenger_amount)
+        valid_pickup_location, pickup_msg = validate_pickup_location(pickup)
+        valid_dropoff_location, dropoff_msg = validate_dropoff_location(dropoff)
+        valid_passenger_amount, pax_msg = validate_passenger_amount(pax)
 
         if not valid_pickup_location:
             messagebox.showerror("Invalid Pick-up Address", pickup_msg, parent=booking_window)
@@ -150,34 +188,19 @@ def booking_window_open():
         dropoff = dropoff_user_entry.get()
         date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         vehicle_type = vehicle.get_vehicle_type()
+        pax = pax_user_entry.get()
         total_distance = get_distance(pickup_user_entry.get(), dropoff_user_entry.get(), api_key )
         total_cost = vehicle.calculate_cost(int(total_distance))
         status = "ongoing"
 
-        if book_now(username, pickup, dropoff, date_and_time, vehicle_type, total_distance, total_cost, status) == True:
-            switch_to_booking_successful_tab()  #Transitions to 3rd tab, the "Successfully Booked" tab.
-
-##########ALISIN ANG SELFFFFFFFFFFFF####################################################################
-
-    # Get input from user
-    def book_ride(self):
-        user = self.user_entry.get()
-        vehicle = self.vehicles[self.vehicle_var.get()]
-        start_location = self.start_entry.get()
-        end_location = self.end_entry.get()
-        passengers = self.passengers_entry.get()
-
+        book_now(username, pickup, dropoff, date_and_time, pax, vehicle_type, total_distance, total_cost, status)
+        
         # VALIDATION OF INPUTS
 
         # Validate if all fields are filled.
         if not user or not start_location or not end_location or not passengers:
             messagebox.showerror("Error", "All fields must be filled.")
             return
-
-        # Check if name inputted is valid.
-        if not user.isascii() or not all(c.isalpha() or c in '.- ' for c in user):
-            messagebox.showerror("Error", "The user name must only contain ASCII letters, dots, or dashes.")
-            return False
 
         # Calculation of distance between two points using Google Maps API.
         try:
@@ -191,32 +214,9 @@ def booking_window_open():
             messagebox.showerror("Error", "Invalid address or location.")
             return
 
-        # Error if passenger is invalid.
-        if not passengers.isnumeric() or int(passengers) == 0:
-            messagebox.showerror("Error", "passengers should be non-negative and non-zero integers.")
-            return
-        
-        # Car's recommended capacity is 4, allow up to 6 passengers only.
-        elif isinstance(vehicle, Car) and 4 < int(passengers) <= 6:
-            if not messagebox.askyesno("Confirmation", "The number of passengers exceeds the car's capacity. Are you sure you want to continue?"):
-                return
-        # Van's recommended capacity is 8, allow up to 12 passengers only.
-        elif isinstance(vehicle, Van) and 8 < int(passengers) <= 12:
-            if not messagebox.askyesno("Confirmation", "The number of passengers exceeds the van's capacity. Are you sure you want to continue?"):
-                return
-
-        # Error if passenger amount is greater than vehicle capacity.
-        elif int(passengers) > vehicle.capacity:
-            messagebox.showerror("Error", "The selected vehicle does not have enough capacity for the number of passengers.")
-            return
-        
-        # Create a booking.
-        booking = Booking(user, vehicle, start_location, end_location, km)
-        self.bookings[booking.no] = booking
-
-        # Insert the booking in the list.
-        self.listbox.insert(tk.END, f"Booking #{booking.no}: {user}")
-        Booking.save_to_file(booking, f"booking_{booking.no}.json")
+# ---------------------------------------------------------------------------- #
+#                 FUNCTIONS FOR CANCEL, SAVE, AND LOAD BOOKINGS                #
+# ---------------------------------------------------------------------------- #
 
     # Widget for cancel Booking
     def cancel_booking(self):
@@ -228,8 +228,8 @@ def booking_window_open():
             Booking.delete_file(f"booking_{booking_id}.json")
             self.listbox.delete(selected)
 
-    def save_bookings(self):
-        for no, booking in self.bookings.items():
+    def save_bookings(booking_window):
+        for no, booking in booking_window.bookings.items():
             filename = f"booking_{no}.json"
             with open(filename, 'w') as f:
                 json.dump(booking.to_dict(), f, indent=4)
@@ -243,8 +243,8 @@ def booking_window_open():
                 self.listbox.insert(tk.END, f"Booking #{booking.no}: {booking.user}")
 
     def on_closing(self):
-        self.save_bookings()
-        self.root.destroy()
+        booking_window.save_bookings(booking_window)
+        booking_window.destroy()
 
 # ---------------------------------------------------------------------------- #
 #                                   FRONTEND                                   #
